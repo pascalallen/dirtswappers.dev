609d2c8db7560df403cf56f1df915f53ed2cd379
pascalallen <thomaspascalallen@yahoo.com>
2016-03-01 17:10:58 -0600

    Finished basic structure of PostsController.

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -2,6 +2,12 @@
 
 class PostsController extends \BaseController {
 
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->beforeFilter('auth', array('except' => array('index', 'show')));
+	}
 	/**
 	 * Display a listing of the resource.
 	 *
@@ -9,7 +15,17 @@ class PostsController extends \BaseController {
 	 */
 	public function index()
 	{
-		//
+		$search = Input::get('search');
+		
+		if ($search) {
+			$query = Post::with('user')->where('title', 'LIKE', '%' . $search . '%')->orWhere('body', 'LIKE', '%' . $search . '%')->orWhere('location', 'LIKE', '%' . $search . '%');
+		} else {
+			$query = Post::with('user');
+		}
+
+		$posts = $query->orderBy('created_at', 'desc')->paginate(30);
+
+		return View::make('posts.index')->with(['posts' => $posts, 'search' => $search]);
 	}
 
 
@@ -20,7 +36,7 @@ class PostsController extends \BaseController {
 	 */
 	public function create()
 	{
-		//
+		return View::make('posts.create');
 	}
 
 
@@ -31,7 +47,10 @@ class PostsController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		$post = new Post();
+		Session::flash('successMessage', 'Your post has been saved.');
+		Log::info(Input::all());
+		return $this->validateAndSave($post);
 	}
 
 
@@ -43,7 +62,12 @@ class PostsController extends \BaseController {
 	 */
 	public function show($id)
 	{
-		//
+		$post = Post::find($id);
+		if(!$post) {
+			App::abort(404);
+		}
+
+		return View::make('posts.show')->with('post', $post);
 	}
 
 
@@ -55,7 +79,11 @@ class PostsController extends \BaseController {
 	 */
 	public function edit($id)
 	{
-		//
+		$post = Post::find($id);
+		if(Auth::id()!= $post->user_id){
+			return Redirect::action('posts.index');
+		}
+		return View::make('posts.edit')->with('post', $post);
 	}
 
 
@@ -67,7 +95,8 @@ class PostsController extends \BaseController {
 	 */
 	public function update($id)
 	{
-		//
+		$post = Post::find($id);
+		return $this->validateAndSave($post);
 	}
 
 
@@ -79,8 +108,43 @@ class PostsController extends \BaseController {
 	 */
 	public function destroy($id)
 	{
-		//
+		$post = Post::find($id);
+		$post->delete();
+		Session::flash('successMessage', 'Your post has been deleted.');
+		return Redirect::action('posts.index');
 	}
 
+	protected function validateAndSave($post)
+	{
+		$validator = Validator::make(Input::all(), Post::$rules);
 
+		if ($validator->fails()) {
+			dd($validator->messages());
+	        // validation failed, redirect to the tutorial create page with validation errors and old inputs
+	        return Redirect::back()->withInput()->withErrors($validator);
+	    } else {
+	    	
+			$post->title = Input::get('title');
+			$post->body = Input::get('body');
+			$post->location = Input::get('location');
+	    	$image = Input::file('image');
+
+			if ($image) {
+				$filename = $image->getClientOriginalName();
+				$tutorial->image = '/uploaded/' . $filename;
+				$image->move('uploaded/', $filename);
+			}
+			
+			$post->user_id = Auth::id();
+
+			$result = $post->save();
+
+			if($result) {
+				Session::flash('successMessage', 'Your tutorial has been saved.');
+				return Redirect::action('PostsController@show', $post->id);
+			} else {
+				return Redirect::back()->withInput();
+			}
+		}
+	}
 }
