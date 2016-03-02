8312a08f65f0e0d913b2582b85c0ba2c6ea4b332
pascalallen <thomaspascalallen@yahoo.com>
2016-03-02 09:10:45 -0600

    Modified create post page.

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -2,12 +2,12 @@
 
 class PostsController extends \BaseController {
 
-	public function __construct()
-	{
-		parent::__construct();
+	// public function __construct()
+	// {
+	// 	parent::__construct();
 
-		$this->beforeFilter('auth', array('except' => array('index', 'show')));
-	}
+	// 	$this->beforeFilter('auth', array('except' => array('index', 'show')));
+	// }
 	/**
 	 * Display a listing of the resource.
 	 *
--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -5,12 +5,8 @@
 @stop
 
 @section('content')
-	
-		
-	<p class="logo">post a tutorial</p>
 
-		<div class = 'content form-div'>
-		{{ Form::open(array('method' => 'post', 'action' => 'TutorialsController@store', 'files' => true)) }}
+	{{ Form::open(array('method' => 'post', 'action' => 'PostsController@store', 'files' => true)) }}
 
 		<div class="form-group {{ ($errors->has('title')) ? 'has-error' : '' }}">
 			{{ $errors->first('title', '<div class="alert alert-danger">:message</div>') }}
@@ -18,10 +14,10 @@
 			{{ Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Enter your title']) }}
 		</div>
 
-		<div class="form-group {{ ($errors->has('content')) ? 'has-error' : '' }}">
-			{{ $errors->first('content', '<div class="alert alert-danger">:message</div>') }}
-			{{ Form::label('content', 'Content') }}
-			{{ Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Enter your content', 'data-provide'=>'markdown', 'data-iconlibrary'=>'fa']) }}
+		<div class="form-group {{ ($errors->has('body')) ? 'has-error' : '' }}">
+			{{ $errors->first('body', '<div class="alert alert-danger">:message</div>') }}
+			{{ Form::label('body', 'Body') }}
+			{{ Form::textarea('body', null, ['class' => 'form-control', 'placeholder' => 'Body']) }}
 		</div>
 
 		<div class="form-group {{ ($errors->has('image')) ? 'has-error' : '' }}">
@@ -30,24 +26,13 @@
 			{{ Form::file('image') }}
 		</div>
 
-		<div class="form-group {{ ($errors->has('video')) ? 'has-error' : '' }}">
-			{{ $errors->first('video', '<div class="alert alert-danger">:message</div>') }}
-			{{ Form::label('video', 'Video') }}
-			{{ Form::file('video', null) }}
+		<div class="form-group {{ ($errors->has('location')) ? 'has-error' : '' }}">
+			{{ $errors->first('location', '<div class="alert alert-danger">:message</div>') }}
+			{{ Form::label('location', 'Location') }}
+			{{ Form::file('location', null) }}
 		</div>
 
-		<div>
-				@foreach($tags as $tag)
-					{{ Form::label('tuttags[]', $tag->name)}}
-					{{ Form::checkbox('tuttags[]', $tag->id)}}
-					<span class="tags-pad"></span>
-				@endforeach
-			<br>
-			{{ Form::label('addtag', "Don't see a tag? Add it here!")}}
-			{{ Form::text('addtag', null,array('id' => 'addtag', 'placeholder'=>'Add a Tag!'))}}
-		</div>
-
-	{{ Form::submit('submit', ['class'=>'gm-button']) }}
+		{{ Form::submit('submit') }}
 	{{ Form::close() }}
 	</div>
 
