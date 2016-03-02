627d0ce1e1ffe4d05ace594ca0c93936e2357053
pascalallen <thomaspascalallen@yahoo.com>
2016-03-01 18:06:22 -0600

    Finished basic structure of master layout.

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>	
+		<title></title>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		{{-- BOOTSTRAP CSS --}}
+		<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+		{{-- CUSTOM CSS --}}
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
+
+		{{-- CUSTOM FONT --}}
+		<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+
+		{{-- FONT AWESOME --}}
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		
+		@yield('top-script')
+	</head>
+	<body>
+		@include('partials.navbar')
+
+		
+
+		@include('partials.footer')
+
+		<!-- JQUERY -->
+		<script src="/js/jquery-2.1.4.min.js"></script>
+
+		<!-- BOOTSTRAP JS -->
+		<script src="/js/bootstrap.min.js"></script>
+
+		<!-- CUSTOM JS BELOW -->
+		<script src="/js/intro.js"></script>
+
+		@yield('bottom-script')
+
+	</body>
