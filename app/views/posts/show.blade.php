41f9ac5203f9586b67976fa612cea9431d912315
pascalallen <thomaspascalallen@yahoo.com>
2016-03-02 22:03:59 -0600

    Finished posts completely, just needs styling.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -2,33 +2,35 @@
 
 @section('top-script')
 
+	<style type="text/css">
+		.row {
+			text-align: center;
+		}
+	</style>
+
 @stop
 
 @section('content')
 
-	<div class="content">
-		@if(Auth::user() == $post->user)
-			<button type="submit" class="btn btn-default" id="edit">Edit Post</button>
-			{{ Form::open(array('action' => array('PostsController@destroy', $post->id, 'files' => true), 'method' => 'DELETE')) }}
-				<button type="submit" class="btn btn-default">Delete</button>
-			{{ Form::close() }}
-		@endif
-
-		<h3 class="title">{{{ ($post->title) }}}</h3>
-		<h5 class="location">{{{ ($post->location) }}}</h3>
-		<p class="body">{{{ ($post->body) }}}</p>
-
-		<div class="row">
-			@if (isset($post->image))
-				<img src="{{{ $post->image }}}" class="col-xs-8 post-image">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			@if(Auth::user() == $post->user)
+				<button type="submit" class="btn btn-default" id="edit">Edit Post</button>
+				{{ Form::open(array('action' => array('PostsController@destroy', $post->id, 'files' => true), 'method' => 'DELETE')) }}
+					<button type="submit" class="btn btn-default">Delete</button>
+				{{ Form::close() }}
 			@endif
+			<h3 class="title">{{{ ($post->title) }}}</h3>
+			<h5 class="location">{{{ ($post->location) }}}</h3>
+			<p class="body">{{{ ($post->body) }}}</p>
+			@if (isset($post->image))
+				<img src="{{{ $post->image }}}">
+			@endif
+			<blockquote>
+				<footer>Created by {{{ $post->user->username }}}, {{{$post->created_at->diffForHumans() }}}</footer>
+			</blockquote>
 		</div>
-
 	</div>
-
-	<blockquote>
-		<footer>Created by {{{ $post->user->username }}}, {{{$post->created_at->diffForHumans() }}}</footer>
-	</blockquote>
 
 @stop
 
