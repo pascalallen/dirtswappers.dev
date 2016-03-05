9be822106d33fefc37642c34aa6647cbe7475d1e
pascalallen <thomaspascalallen@yahoo.com>
2016-03-05 14:40:19 -0600

    Working on users.show page.

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -2,6 +2,14 @@
 
 @section('top-script')
 
+		<style type="text/css">
+
+			.row {
+				text-align: center;
+			}
+
+		</style>
+
 @stop
 
 @section('content')
@@ -12,27 +20,31 @@
 				{{{ $user->username }}}
 				<img src="{{{ $user->image }}}">
 			@else
-				<h1>Let's Do This, {{{ $user->username }}}!</h1>
+				<h1>Let's do this, {{{ $user->username }}}!</h1>
+				<br>
 				<img src="{{{ $user->image }}}">
+				<br>
+				<br>
+				<a href="{{{action('PostsController@create')}}}"><i class="fa fa-pencil fa-3x"></i></a>
 			@endif
 			<br>
-			{{{ $user->username }}}'s Content</h1>
+			<h1>{{{ $user->username }}}'s Content</h1>
 
-				<table>
-					@foreach($user->posts()->get() as $post)
-						<tr>
-							<td><a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></td>
-
-							@if(Auth::id()== $user->id)
-							<td><a href="{{{ action('PostsController@edit', $post->id) }}}"><button class = "gm-button">Edit</button></a></td>
-
-							{{ Form::open(array('action' => array('PostsController@destroy', $post->id, 'files' => true), 'method' => 'DELETE')) }}
-							<td><button class = "gm-button">Delete</button></td>
-							{{Form::close()}}
-							@endif
-						</tr>
+					@foreach($user->post()->get() as $post)
+						<table class="table">
+							<tbody>
+								<tr>
+									<td><a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></td>
+									@if(Auth::id() == $user->id)
+										<td><a href="{{{ action('PostsController@edit', $post->id) }}}">{{ Form::submit('Edit', ['class' => 'btn btn-warning']) }}</a></td>
+										{{ Form::open(array('action' => array('PostsController@destroy', $post->id, 'files' => true), 'method' => 'DELETE')) }}
+											<td>{{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}</td>
+										{{Form::close()}}
+									@endif
+								</tr>
+							</tbody>
+						</table>
 					@endforeach
-				</table>
 		</div>
 	</div>
 
